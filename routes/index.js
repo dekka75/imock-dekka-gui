566482494512940f99cedbe6a92509dc11c43021
dekka75 <didier.kimes@gmail.com>
2016-11-18 20:07:46 +0100

    Add lodash module

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,44 @@
 var debug = require('debug')('imock:gui:routes:index')
 var express = require('express')
 var request = require("request")
+var _ = require('lodash')
 var router = express.Router()
 
-/* GET users listing. */
+/* Dashboard view */
 router.get('/', function (req, res, next) {
     var server = req.app.locals.server
     var options = req.app.locals.options
 
-    // GET Groups
-    options.uri = server + '/api/groups'
-    request(options, function (err, response, groups) {
+    // Get groups list
+    options.uri = server + '/api/services/all'
+    request(options, function (err, response, body) {
         if (err) {} else {
+            var groups = JSON.parse(body)
+            var groupsList = _.uniq(_.map(groups, 'group'))
+            var group = groupsList[0]
+
+            // Group by group / campaign
+            var servicesList = {};
+            groups.forEach(function (currentService) {
+                var group = currentService['group']
+                var campaign = currentService['campaign']
+                if (!servicesList[group]) {
+                    servicesList[group] = []
+                }
+                if (!servicesList[group][campaign]) {
+                    servicesList[group][campaign] = []
+                }
+                servicesList[group][campaign].push(currentService)
+            })
+
             res.set('Content-Type', 'text/html; charset=UTF-8')
             res.status(200)
             res.render('index', {
                 name: 'iMock',
                 title: 'Virtual Services',
-                groups: JSON.parse(groups)
+                active: group,
+                groupsList: groupsList,
+                servicesList: servicesList
             })
         }
     })
